3345af145910107e52bdbf3128849511bb6f0a15
EstelleMorelli <estelle.morelli@oclock.school>
2024-07-15 15:48:34 +0200

    modification d'axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,17 +1,9 @@
 import axios from 'axios';
 
 // on créé une instance axios (comme un duplicata) avec la base url pré configuré pour ne pas la répéter à chaque fois
-export const axiosInstance = axios.create({
-  baseURL: 'http://amidon.yoan80490-server.eddi.cloud/api/',
+const axiosInstance = axios.create({
   // baseURL: 'http://localhost/Spe/projet-4-aide-o-tri-back/public/api/',
+  withCredentials: true,
 });
 
-// cette fonction ajoute le token dans les entetes de toutes les requetes qui seront lancée avec l'instance, on l'execute dès qu'on reçoit le token du back (quand on est connecté)
-export const addTokenToAxiosInstance = (token: string) => {
-  axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
-
-// cette fonction enleve le token des entetes de toutes les requetes qui seront lancée avec l'instance, on l'execute dès qu'on se deconnecte
-export const removeTokenFromAxiosInstance = () => {
-  axiosInstance.defaults.headers.common.Authorization = ``;
-};
+export default axiosInstance;
