aec3009b1799b35bf55bee624b43695d1baa49e8
EstelleMorelli <estelle.morelli@oclock.school>
2024-07-16 16:12:05 +0200

    changement route api

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // on créé une instance axios (comme un duplicata) avec la base url pré configuré pour ne pas la répéter à chaque fois
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost/Spe/projet-4-aide-o-tri-back/public/api/',
+  baseURL: 'https://amidon.dev-me.fr/Back/public/api/',
   withCredentials: true,
 });
 
