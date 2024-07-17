f9dd362d12bcb1a01d94d9170fa1dddf1d7db9db
Estelle Morelli <157108643+EstelleMorelli@users.noreply.github.com>
2024-07-17 11:33:45 +0200

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // on créé une instance axios (comme un duplicata) avec la base url pré configuré pour ne pas la répéter à chaque fois
 const axiosInstance = axios.create({
-  baseURL: 'https://amidon.dev-me.fr/Back/public/api/',
+  baseURL: 'https://amidon.dev-me.fr/Amidon/public/api/',
 // ??
   withCredentials: true,
 });
