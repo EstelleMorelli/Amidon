48816246484acee746ec7bd0e212285fa50e8504
MarionLeblanc77 <157643710+MarionLeblanc77@users.noreply.github.com>
2024-08-30 16:33:27 +0200

    Update data.ts

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,17 +1,17 @@
 // VERSION LOCALE
 
-export const baseUserPictureURL =
-  // 'http://amidon.yoan80490-server.eddi.cloud/assets/images/users';
-  'http://localhost/Spe/projet-4-aide-o-tri-back/public/assets/images/users';
+// export const baseUserPictureURL =
+//   // 'http://amidon.yoan80490-server.eddi.cloud/assets/images/users';
+//   'http://localhost/Spe/projet-4-aide-o-tri-back/public/assets/images/users';
 
-export const baseProductPictureURL =
-  // 'http://amidon.yoan80490-server.eddi.cloud/assets/images/products';
-  'http://localhost/Spe/projet-4-aide-o-tri-back/public/assets/images/products';
+// export const baseProductPictureURL =
+//   // 'http://amidon.yoan80490-server.eddi.cloud/assets/images/products';
+//   'http://localhost/Spe/projet-4-aide-o-tri-back/public/assets/images/products';
 
 // VERSION PROD INFOMANIAK
 
-// export const baseUserPictureURL =
-//   'https://amidon.dev-me.fr/Amidon/public/assets/images/users';
+export const baseUserPictureURL =
+  'https://amidon.dev-me.fr/Amidon/public/assets/images/users';
 
-// export const baseProductPictureURL =
-// 'https://amidon.dev-me.fr/Amidon/public/assets/images/products';
+export const baseProductPictureURL =
+'https://amidon.dev-me.fr/Amidon/public/assets/images/products';
