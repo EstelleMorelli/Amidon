a03d19b9a293a6813adee35dece7140f959b197d
MarionLeblanc77 <157643710+MarionLeblanc77@users.noreply.github.com>
2024-08-30 16:34:48 +0200

    Update login.ts

--- src/store/middlewares/login.ts
+++ src/store/middlewares/login.ts
@@ -6,7 +6,7 @@ import type { RootState } from '../store';
 import axiosInstance from '../../utils/axios';
 
 // À DÉCOMMENTER POUR UN USAGE EN LOCAL - COMMENTER POUR INFOMANIAK
-import { addTokenToAxiosInstance } from '../../utils/axios';
+// import { addTokenToAxiosInstance } from '../../utils/axios';
 
 // Notre action asynchrone qui va faire l'appel API
 const login = createAsyncThunk(
@@ -25,7 +25,7 @@ const login = createAsyncThunk(
 
       // Appel notre fonction qui met le token envoyé par le back dans l'instance Axios pour le renvoyer à chaque requête dans l'entête
       // À DÉCOMMENTER POUR UN USAGE EN LOCAL - COMMENTER POUR INFOMANIAK
-      addTokenToAxiosInstance(result.data.token);
+      // addTokenToAxiosInstance(result.data.token);
 
       const result2 = await axiosInstance.post('/login_check', {
         email,
