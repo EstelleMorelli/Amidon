ce79436dc72176b34c870e99bff9a9596ac34330
Marion Leblanc <marion.leblanc77@gmail.com>
2024-09-26 12:33:30 +0200

    connexion params for local

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,5 @@
 // VERSION LOCALE
-// import axios from 'axios';
+import axios from 'axios';
 
 // On crée une instance axios (comme un duplicata) avec la base url pré configuré pour ne pas la répéter à chaque fois
 export const axiosInstance = axios.create({
@@ -8,17 +8,17 @@ export const axiosInstance = axios.create({
   baseURL: 'http://amidonbacknem/api',
 });
 
-// // cette fonction ajoute le token dans les entetes de toutes les requetes qui seront lancée avec l'instance, on l'execute dès qu'on reçoit le token du back (quand on est connecté)
-// export const addTokenToAxiosInstance = (token: string) => {
-//   axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
-// };
+// cette fonction ajoute le token dans les entetes de toutes les requetes qui seront lancée avec l'instance, on l'execute dès qu'on reçoit le token du back (quand on est connecté)
+export const addTokenToAxiosInstance = (token: string) => {
+  axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
-// // cette fonction enleve le token des entetes de toutes les requetes qui seront lancée avec l'instance, on l'execute dès qu'on se deconnecte
-// export const removeTokenFromAxiosInstance = () => {
-//   axiosInstance.defaults.headers.common.Authorization = ``;
-// };
+// cette fonction enleve le token des entetes de toutes les requetes qui seront lancée avec l'instance, on l'execute dès qu'on se deconnecte
+export const removeTokenFromAxiosInstance = () => {
+  axiosInstance.defaults.headers.common.Authorization = ``;
+};
 
-// export default axiosInstance;
+export default axiosInstance;
 
 // VERSION PROD INFOMANIAK
 // import axios from 'axios';
@@ -32,4 +32,3 @@ export const axiosInstance = axios.create({
 // });
 
 // export default axiosInstance;
-
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,19 +1,15 @@
 // VERSION LOCALE
-
-
 export const baseUserPictureURL =
   // 'http://amidon.yoan80490-server.eddi.cloud/assets/images/users';
   // 'http://localhost/Spe/projet-4-aide-o-tri-back/public/assets/images/users';
-     'http://amidonbacknem/assets/images/users';
+  'http://amidonbacknem/assets/images/users';
 
 export const baseProductPictureURL =
   // 'http://amidon.yoan80490-server.eddi.cloud/assets/images/products';
   // 'http://localhost/Spe/projet-4-aide-o-tri-back/public/assets/images/products';
-     'http://amidonbacknem/assets/images/products';
-
+  'http://amidonbacknem/assets/images/products';
 
 // VERSION PROD INFOMANIAK
-
 // export const baseUserPictureURL =
 //   'https://amidon.dev-me.fr/Amidon/public/assets/images/users';
 
