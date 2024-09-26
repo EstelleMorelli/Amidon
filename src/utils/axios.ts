c94690ae8b6ee0c1a9f94dac599115c1d2bcf8af
Marion Leblanc <marion.leblanc77@gmail.com>
2024-09-26 09:06:55 +0200

    maj params local

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,8 +4,8 @@ import axios from 'axios';
 // On crée une instance axios (comme un duplicata) avec la base url pré configuré pour ne pas la répéter à chaque fois
 export const axiosInstance = axios.create({
   // baseURL: 'http://amidon.yoan80490-server.eddi.cloud/api/',
-  // baseURL: 'http://localhost/Spe/projet-4-aide-o-tri-back/public/api/',
-  baseURL: 'http://amidonbacknem/api',
+  baseURL: 'http://localhost/Spe/projet-4-aide-o-tri-back/public/api/',
+  // baseURL: 'http://amidonbacknem/api',
 });
 
 // cette fonction ajoute le token dans les entetes de toutes les requetes qui seront lancée avec l'instance, on l'execute dès qu'on reçoit le token du back (quand on est connecté)
